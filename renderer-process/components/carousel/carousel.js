64ab218f4477a4c5cdc793cc0889c5f062469371
Jana Koutnikova <janca.koutnikova@gmail.com>
2021-06-10 23:48:00 +0200

    Clock-modal

--- renderer-process/components/carousel/carousel.js
+++ renderer-process/components/carousel/carousel.js
@@ -3,21 +3,23 @@ import { NewsArticle } from '../news-article/news-article.js';
 export class Carousel extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `<header class="header-news">
-                            <div class="header-news__container">
-                            </div>
-                            <button id="carousel-button-left">
-                                <i class="fas fa-chevron-left"></i>
-                            </button>
-                            <button id="carousel-button-right" class="last">
-                                <i class="fas fa-chevron-right"></i>
-                            </button>
-                          </header>`
+        this.innerHTML = `
+        <header class="header-news">
+            <div class="header-news__container">
+            </div>
+            <button id="carousel-button-left">
+                <i class="fas fa-chevron-left"></i>
+            </button>
+            <button id="carousel-button-right" class="last">
+                <i class="fas fa-chevron-right"></i>
+            </button>
+        </header>
+        `;
+
         this.header = document.querySelector('header.header-news > div.header-news__container');
         this.carouselItemCount = 2;
         this.carouselItemStart = 0;
         this.articles;
-    
 
         this.buttonLeft = document.querySelector('#carousel-button-left');
         this.buttonRight = document.querySelector('#carousel-button-right');
@@ -37,7 +39,7 @@ export class Carousel extends HTMLElement {
         this.articles = news;
         this.header.innerText = '';
         for(let i = this.carouselItemStart; i < (this.carouselItemStart + this.carouselItemCount); i ++) {
-            const newsValue = news[i];
+            const newsValue = this.articles[i];
             const newsArticle = new NewsArticle(newsValue);
             this.header.appendChild(newsArticle);
         }
--- renderer-process/main.js
+++ renderer-process/main.js
@@ -1,76 +1,177 @@
-import { Day } from './components/day/day.js';
 import { Carousel } from './components/carousel/carousel.js';
+import { Day } from './components/day/day.js';
 
 const carousel = document.querySelector('app-carousel');
+
 
 fetch('http://localhost:3000/news.json')
     .then(serverResponse => serverResponse.text())
     .then(responseText => {
-        const data = JSON.parse(responseText);        
-        carousel.populateNewsCarousel(data.articles);
+      const data = JSON.parse(responseText);
+      carousel.populateNewsCarousel(data.articles);
     });
 
 const mainContent = document.querySelector('section.main-content');
-for (let i = 1; i < 31; i++) {
-    mainContent.appendChild(new Day(i));
+
+const currentDate = new Date();
+const maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+
+for (let i = 1; i <= maxDate; i++) {
+    const dayDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
+    mainContent.appendChild(new Day(dayDate));
 }
 
 
 
+function showDayModal() {
+    const template = document.querySelector('#modal-template');
+    const modal = template.content.cloneNode(true);
+    const closeAction = () => {
+        const child = document.querySelector('section.modal-container');
+        document.body.removeChild(child);
+    };
+    modal.querySelector('#close-modal').addEventListener('click', closeAction);
+    const cancelButton = modal.querySelector('#cancel-button');
+    cancelButton.addEventListener('click', closeAction);
+
+    modal.querySelector('#save-button').addEventListener('click', () => {
+      const formRef = document.querySelector('#modal-form');
+      const formData = new FormData(formRef);
+      const data = formData.entries();
+
+      const object = {};
+
+      for (let formValue of data) {
+        const key = formValue[0];
+        const value = formValue[1];
+        object.key = value;
+        object['key'] = value;
+        object[key] = value;
+      }
+      
+
+  });
+
+   
+
+    document.body.appendChild(modal);
+    
+    const checkbox = document.querySelector('#limitAttendeesByGender');
+    const genderRole = document.querySelector('#genderSelectRow');
+    checkbox.addEventListener('change', (event) => {
+        if (event.target.checked){
+            genderRole.classList.remove('hidden')
+        } else {
+            genderRole.classList.add('hidden')
+        }
+    })
+    
+    let contactsArray;
+    fetch('http://localhost:3000/contacts')
+        .then(serverResponse => serverResponse.text())
+        .then(responseText => {
+        contactsArray = JSON.parse(responseText);      
+        createOptions(contactsArray);
+        
+    });
 
 
+    const radioButtons = document.querySelectorAll('#genderSelectRow > input')
+    
+    for (let radio of radioButtons) {
+      radio.addEventListener('change', () => {
+          const formRef = document.querySelector('#modal-form');
+          const formData = new FormData(formRef);
+          const gender = formData.get('gender');
+          const filteredContacts = contactsArray.filter((contact) => {
+              return contact.gender === gender;
+          });
+          createOptions(filteredContacts);
+      });
+  }
 
 
+  }
+function createOptions(contactsArray) {
+        const select = document.querySelector('#eventAttendees');
+        
 
+        const oldOptions = document.querySelectorAll('.hakunamatata');
+        oldOptions.forEach(opt => {
+          // opt.remove();
+          select.removeChild()
+        })
 
+        // select.innerHTML = '<option value=" "></option>';
 
-
-
-// ukazkovy kod:
-const text = 'ashdf';
-const cislo = 3;
-
-const automobil = {
-    pocetMistKSezeni: 5,
-    barvaKaroserie: 'cervena'
-};
-
-console.log(automobil.barvaKaroserie);
-
-class Operenec {
-
-    constructor(volani) {
-        this.zvuk = volani;
-    }
-
-    vydejZvuk() {
-        console.log('delka zvuku', this.zvuk.length);
-        console.log(this.zvuk);
-    }
+        contactsArray.forEach(it => {
+            const option = document.createElement('option');
+            option.setAttribute('value', it.id);
+            option.innerText = `${it.first_name} ${it.last_name}`;
+            option.classList.add('hakunamatata');
+            select.appendChild(option);
+        });
 
 }
 
-class Kacer extends Operenec {
-    constructor() {
-        super('kva kva');
-        console.log(this.zvuk);
-    }
 
-    plavPoJezirku() {
-        console.log('plavu plavu');
-    }
+window.showModal = showDayModal;
+
+
+
+
+
+
+
+
+
+
+
+
+// *** DOMÁCÍ ÚKOL 5 *** //
+
+
+
+const showTime = () => {
+
+  const clockContainer = document.querySelector('.clock-body');
+  clockContainer.innerText = new Date().toLocaleTimeString();
+  
+  const showTimer = () => {    
+    clockContainer.innerText = new Date().toLocaleTimeString();
+  }
+  
+  const ticking = setInterval(() => showTimer() ,
+   1000)
+  
+  const closeAction = () => {
+    clearInterval(ticking);
+    const child = document.querySelector('section.clock-container');
+    document.body.removeChild(child);    
+  };
+
+  setTimeout(() => {      
+    closeAction();  
+  }, 5000);
 }
 
-class Kohout extends Operenec {
-    hlasVychodSlunce() {
-        console.log('vychazi');
-    }
+const showClockModal = () => {
+  const templateModal = document.querySelector('#clock-modal');
+  const clockModal = templateModal.content.cloneNode(true);
+  document.body.appendChild(clockModal); 
+  showTime();  
 }
 
-const kacer = new Kacer();
+let password = '';
+window.addEventListener('keydown', (keyboardEvent) => { 
+           
+        password = password + keyboardEvent.key;
+        password = password.slice(-4);
+        console.log (password)
+        if (password === 'time') {          
+          showClockModal();            
+        };
+})
 
-kacer.vydejZvuk();
 
-const kohout = new Kohout('kikiriki');
 
-kohout.vydejZvuk();
