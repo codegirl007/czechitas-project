bb9e8a3d85cc7144b9d3eb97286af4466fc43d9c
Jana Koutnikova <janca.koutnikova@gmail.com>
2021-06-21 11:19:13 +0200

    Add ukol_6

--- renderer-process/main.js
+++ renderer-process/main.js
@@ -1,8 +1,10 @@
 import { Carousel } from './components/carousel/carousel.js';
 import { Day } from './components/day/day.js';
 
-const carousel = document.querySelector('app-carousel');
 
+
+// ***CAROUSEL HEADER***
+const carousel = document.querySelector('app-carousel');
 
 fetch('http://localhost:3000/news.json')
     .then(serverResponse => serverResponse.text())
@@ -11,21 +13,137 @@ fetch('http://localhost:3000/news.json')
       carousel.populateNewsCarousel(data.articles);
     });
 
+
 const mainContent = document.querySelector('section.main-content');
 
-const currentDate = new Date();
-const maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+// const currentDate = new Date();
 
-for (let i = 1; i <= maxDate; i++) {
-    const dayDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
-    mainContent.appendChild(new Day(dayDate));
+// const maxDate = new Date(
+//   currentDate.getFullYear(),
+//   currentDate.getMonth() + 1, 0).getDate();
+//   console.log(maxDate)
+
+// for (let i = 1; i <= maxDate; i++) {
+//     const dayDate = new Date(
+//       currentDate.getFullYear(), 
+//       currentDate.getMonth(), i);
+//     mainContent.appendChild(new Day(dayDate));
+// }
+
+
+// ***DOMÁCÍ ÚKOL 6 - SELECTS FOR MONTH, YEAR ***
+
+const selectsDiv = document.querySelector('.selects'); 
+
+let CURRENT_YEAR = new Date().getFullYear();
+let CURRENT_MONTH = new Date().getMonth();
+let CURRENT_DATE = new Date(CURRENT_YEAR, CURRENT_MONTH);
+
+const MONTHS = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"];
+const MINYEAR = CURRENT_YEAR - 80;
+const MAXYEAR = CURRENT_YEAR + 20;
+
+populateDays(CURRENT_YEAR, CURRENT_MONTH);
+
+const selectMonth = document.createElement('select');
+selectMonth.classList.add('select-months');
+selectsDiv.appendChild(selectMonth);
+
+MONTHS.forEach(month  => {
+  const option = document.createElement('option');
+  option.setAttribute('value', month);
+  option.innerText = `${month}`;
+  option.classList.add('month');
+  selectMonth.appendChild(option);
+  if (month === MONTHS[CURRENT_MONTH]) {
+    option.setAttribute('selected', 'selected');
+  }
+})
+
+const selectYear = document.createElement('select');
+selectYear.classList.add('select-years');
+selectsDiv.appendChild(selectYear);
+
+for (let i = MINYEAR; i < MAXYEAR + 1; i++ ) {
+  const option = document.createElement('option');
+  option.setAttribute('value', i);
+  option.innerText = i;
+  option.classList.add('year');
+  selectYear.appendChild(option);
+  if (i === CURRENT_YEAR) {
+    option.setAttribute('selected', 'selected');
+  }
 }
 
+selectMonth.addEventListener('change', event => {
+  
+  const CURRENT_MONTH = MONTHS.indexOf(event.target.value);
+  console.log(CURRENT_MONTH)
+  removeDays();
+  populateDays(CURRENT_YEAR, CURRENT_MONTH); 
+})
+
+selectYear.addEventListener('change', event => {
+  const selectedYearString = event.target.value;
+  CURRENT_YEAR = parseInt(selectedYearString);
+  console.log(CURRENT_YEAR)
+  removeDays();
+  populateDays(CURRENT_YEAR, CURRENT_MONTH);  
+})
+
+function removeDays () {
+  document.querySelectorAll("app-day").forEach(day => day.parentNode.removeChild(day));
+}
+
+function populateDays (currentyear, currentmonth) {
+
+   CURRENT_DATE = new Date(currentyear, currentmonth);
+
+  let maxDate = new Date(
+    CURRENT_DATE.getFullYear(),
+    CURRENT_DATE.getMonth() + 1, 0).getDate();
+    console.log(maxDate);  
+  
+  for (let i = 1; i <= maxDate; i++) {
+      const dayDate = new Date(
+        CURRENT_DATE.getFullYear(), 
+        CURRENT_DATE.getMonth(), i);
+      mainContent.appendChild(new Day(dayDate));
+      let CURRENT_NUMBER = new Date().getDate();
+      console.log(CURRENT_NUMBER)
+
+      const divDays = document.querySelectorAll("#day-number");
+      for (let divDay of divDays) {
+          divDay.id = dayDate.getDate();
+          if ((divDay.id == new Date().getDate()) && currentmonth == new Date().getMonth() && currentyear == new Date().getFullYear()) 
+          { divDay.parentNode.style.backgroundColor = "red" }
+      }      
+  }
+}
+
+ 
+// const currentDate = new Date()
+
+// const maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+
+// for(let i = 1; i <= maxDate; i++){
+//     const dayDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), i);
+//     mainContent.appendChild(new Day(dayDate));
+// }
 
 
+//     const dayDate = new Date(
+//       currentDate.getFullYear(), 
+//       currentDate.getMonth(), i);
+//     mainContent.appendChild(new Day(dayDate));
+
+
+
+// ***DAY MODAL***
 function showDayModal() {
     const template = document.querySelector('#modal-template');
     const modal = template.content.cloneNode(true);
+    
     const closeAction = () => {
         const child = document.querySelector('section.modal-container');
         document.body.removeChild(child);
@@ -33,6 +151,7 @@ function showDayModal() {
     modal.querySelector('#close-modal').addEventListener('click', closeAction);
     const cancelButton = modal.querySelector('#cancel-button');
     cancelButton.addEventListener('click', closeAction);
+
 
     modal.querySelector('#save-button').addEventListener('click', () => {
       const formRef = document.querySelector('#modal-form');
@@ -44,12 +163,13 @@ function showDayModal() {
       for (let formValue of data) {
         const key = formValue[0];
         const value = formValue[1];
-        object.key = value;
-        object['key'] = value;
+        // object.key = value;
+        // object['key'] = value;
         object[key] = value;
+        // object.gender = 'Female'
       }
       
-
+      const isHoliday = formData.get('isHolidayControl') === 'on';
   });
 
    
@@ -59,7 +179,7 @@ function showDayModal() {
     const checkbox = document.querySelector('#limitAttendeesByGender');
     const genderRole = document.querySelector('#genderSelectRow');
     checkbox.addEventListener('change', (event) => {
-        if (event.target.checked){
+        if (event.target.checked === true){
             genderRole.classList.remove('hidden')
         } else {
             genderRole.classList.add('hidden')
@@ -72,11 +192,11 @@ function showDayModal() {
         .then(responseText => {
         contactsArray = JSON.parse(responseText);      
         createOptions(contactsArray);
-        
+        console.log(contactsArray)
     });
 
 
-    const radioButtons = document.querySelectorAll('#genderSelectRow > input')
+    const radioButtons = document.querySelectorAll('#genderSelectRow > input');
     
     for (let radio of radioButtons) {
       radio.addEventListener('change', () => {
@@ -94,12 +214,13 @@ function showDayModal() {
   }
 function createOptions(contactsArray) {
         const select = document.querySelector('#eventAttendees');
+        // select.innerHTML = '<option value = " "></option>'
         
 
-        const oldOptions = document.querySelectorAll('.hakunamatata');
+        const oldOptions = document.querySelectorAll('.generated-option');
         oldOptions.forEach(opt => {
           // opt.remove();
-          select.removeChild()
+          select.removeChild(opt)
         })
 
         // select.innerHTML = '<option value=" "></option>';
@@ -108,7 +229,7 @@ function createOptions(contactsArray) {
             const option = document.createElement('option');
             option.setAttribute('value', it.id);
             option.innerText = `${it.first_name} ${it.last_name}`;
-            option.classList.add('hakunamatata');
+            option.classList.add('generated-option');
             select.appendChild(option);
         });
 
@@ -117,18 +238,7 @@ function createOptions(contactsArray) {
 
 window.showModal = showDayModal;
 
-
-
-
-
-
-
-
-
-
-
-
-// *** DOMÁCÍ ÚKOL 5 *** //
+// *** DOMÁCÍ ÚKOL 5 SHOWTIME*** //
 
 
 
@@ -172,6 +282,3 @@ window.addEventListener('keydown', (keyboardEvent) => {
           showClockModal();            
         };
 })
-
-
-
